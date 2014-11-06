7cbfc535effdea967aab443c19572457ff33b7fe
anjankumarhn <aknagaraja@qwinixtech.com>
2014-11-06 12:28:17 +0530

    Fixing heroku UI issue

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,8 @@ module MadhuraMandya
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-      config.assets.initialize_on_precompile = false
+      config.assets.initialize_on_precompile = true
+      config.assets.version='1.0' => '1.1'
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
